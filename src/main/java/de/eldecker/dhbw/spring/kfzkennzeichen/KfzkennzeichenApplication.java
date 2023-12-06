acc9bee7e5867b87603d077df37204b9f0a5acf7
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-12-06 16:45:10 +0100

    Projekt von start.spring.io

---
+++ src/main/java/de/eldecker/dhbw/spring/kfzkennzeichen/KfzkennzeichenApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.kfzkennzeichen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfzkennzeichenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfzkennzeichenApplication.class, args);
+	}
+
+}
