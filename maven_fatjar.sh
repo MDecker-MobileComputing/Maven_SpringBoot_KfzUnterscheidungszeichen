29395b66c2181e04c2483aaa70fa4dfedeb75553
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-12-07 20:55:44 +0100

    Einige maven_*.sh-Skripte dazu; public/index.html dazu, wird als statischer Content ausgeliefert.

---
+++ maven_fatjar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+
+./mvnw clean package
+# "package"-Phase erstellt bei Spring-Boot ohne zusaetzliche Konfig
+# ein FatJar
+
+echo
+
---
+++ src/main/java/de/eldecker/dhbw/spring/kfzkennzeichen/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Hauptpaket für Spring-Boot-Anwendung mit REST-API zur Abfrage
+ * von Unterscheidungszeichen von deutschen KFZ-Kennzeichen.
+ */
+package de.eldecker.dhbw.spring.kfzkennzeichen;
